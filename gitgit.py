eb5c5bfa0cd7fa5feebdeeda296d09f2420c54e5
eeuoo <lee512joo@gmail.com>
2018-11-15 13:10:24 +0900

    new 2018-11-15

--- gitgit.py
+++ gitgit.py
@@ -3,6 +3,7 @@ import datetime
 
 now = datetime.datetime.now()
 
+# cmd창에서 실행하려면 sys가 있어야 하지만, Visual Studio에선 실행 가능
 
 default_message = "new {}".format(now.strftime('%Y-%m-%d'))
 commit_msg = default_message
