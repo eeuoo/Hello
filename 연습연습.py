4a2633b12f08a8c8472c17fb5f61a1c2afb4bdc8
eeuoo <lee512joo@gmail.com>
2018-11-16 14:15:54 +0900

    new 2018-11-16

--- 연습연습.py
+++
@@ -1,9 +0,0 @@
-import random
-
-deck = ['s2','s3','s4','s5','s6','s7','s8','s9','s10','c2','c3','c4','c5','c6','c7','c8','c9','c10','h2','h3','h4','h5','h6','h7','h8','h9','h10','d2','d3','d4','d5','d6','d7','d8','d9','d10']
-
-card = random.choice(deck)
-
-print(card)
-
-print("안녕하세요 안녕히가세요")
