75bbf05ddc7ceb8be84fea1e3f39078f271eed04
eeuoo <lee512joo@gmail.com>
2019-01-14 15:55:41 +0900

    1월 14일

--- crawl/ticket_get_idHall.py
+++ crawl/ticket_get_idHall.py
@@ -5,22 +5,26 @@ from bs4 import BeautifulSoup
 session = requests.session()
 
 
-url = "http://ticket.yes24.com/Pages/Perf/Sale/PerfSaleProcess.aspx?IdPerf=32098"
+url = "https://www.yes24.com/Templates/FTLogin.aspx?ReturnURL=http://ticket.yes24.com/Pages/Perf/Detail/DetailSpecial.aspx&&ReturnParams=IdPerf=32098"
 
-headers =  { "Referer" : "http://ticket.yes24.com/Pages/Perf/Sale/PerfSaleProcess.aspx?IdPerf=32098" 
-}
+headers =  { "Referer" : url }
 
-# params = {
-#     "SMemeberID" : "dlguswn512" ,
-#     "SMemeberPassword" : "lhzoo4295"
-# }
+params = {
+    "SMemberID" : "dlguswn512" ,
+    "SMemberPassword" : "lhzoo4295"
+ }
 
-res = session.post(url, headers = headers)
+res = session.post(url, data = params, headers = headers)
 res.raise_for_status()
 
-soup = BeautifulSoup(res.text, "html.parser")
+res2 = session.get('http://ticket.yes24.com/Pages/Perf/Sale/PerfSaleProcess.aspx?IdPerf=32098')
+res.raise_for_status()
+
+soup = BeautifulSoup(res2.text, "html.parser")
+
 print(soup)
 exit()
+
 script = soup.find('ul#ulTime')
 
 print(script)
