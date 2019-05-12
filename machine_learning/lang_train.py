f5bf211d7e118a9b4b532297d2954552b0655274
eeuoo <lee512joo@gmail.com>
2019-05-12 23:57:33 +0900

    2919-05-12

--- machine_learning/lang_train.py
+++ machine_learning/lang_train.py
@@ -27,3 +27,36 @@ def check_freq():
     return (freq, lang)
 
 
+# 각 파일 처리
+def load_files(path):
+    freqs = []
+    labels = []
+    file_list = glob.glob(path)
+
+    for fname in file_list:
+        r = check_freq(fname)
+        freqs.append(r[0])
+        labels.append(r[1])
+    return { "freqs" : freqs, "labels" : labels }
+
+data = load_files("./lang/train/*.txt") # 학습 데이터
+test = load_files("./lang/test/*.txt")  # 검증 데이터
+
+
+# json으로 결과 저장
+with open("./lang/freq.json", "w", encoding="utf-8") as fp :
+    json.dump([data, test], fp)
+
+# 학습
+clf = svm.SVC()
+clf.fit( data["freqs"], data["labels"] )
+
+# 예측
+predict = clf.predict( test["freqs"] )
+
+# 결과 테스트
+ac_score = metrics.accuracy_score( test["labels"]. predict )
+cl_report = metrics.classification_report( test["labels"], predict )
+
+print( "정답률= ", ac_score )
+print( "리포트= ", cl_report)
