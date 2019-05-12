b73a7e0ac77c9468096527e5fd7b919fac058593
eeuoo <lee512joo@gmail.com>
2019-05-12 23:49:10 +0900

    2919-05-12

--- machine_learning/lang_train.py
+++ machine_learning/lang_train.py
@@ -10,3 +10,20 @@ def check_freq():
     text = text.lower()
 
     # 숫자 세기 변수 초기화
+    cnt = [0 for n in range(0, 26)]
+    code_a = ord("a")
+    code_z = ord("z")
+
+    # 알파벳 출현 횟수 구하기
+    for ch in text :
+        n = ord(ch)
+        if code_a <= n <= code_z:
+            cnt[n - code_a] += 1
+
+    
+    # 정규화
+    total = sum(cnt)
+    freq = list(map(lambda n : n / total, cnt))
+    return (freq, lang)
+
+
