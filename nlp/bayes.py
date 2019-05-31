905c1c32ce8d5774070e23efab8984bb1823334d
eeuoo <lee512joo@gmail.com>
2019-05-31 22:16:22 +0900

    2019-05-31

--- nlp/bayes.py
+++ nlp/bayes.py
@@ -32,6 +32,60 @@ class BayesianFilter :
         self.word_dict[category][word] += 1
         self.words.add(word)
 
+    def inc_category(self, category) :
+        # 카테고리 계산하기
+        if not category in self.category_dict :
+            self.category_dict[category] = 0
+        self.category_dict[category] += 1
+
+    # 텍스트 학습하기
+    def fit(self, text, category) :
+        ''' 텍스트 학습 '''
+        word_list = self.split(text) 
+        for word in word_list :
+            self.inc_word(word, category)
+        self.inc_category(category)
+    
+    # 단어 리스트에 점수 매기기
+    def score(self, words, category) :
+        score = math.log(self.category_prob(category))
+        for word in words :
+            score += math.log(self.word_prob(word, category))
+        return score
+
+    # 예측하기
+    def predict(self, text) :
+        best_category = None 
+        max_score = -sys.maxsize
+        words = self.split(text)
+        score_list = []
+        for category in self.category_dict.keys() :
+            score = self.score(words, category)
+            score_list.append((category, score))
+            if score > max_score :
+                max_score = score
+                best_category = category
+        return best_category, score_list
+
+    # 카테고리 내부의 단어 출현 횟수 구하기
+    def get_word_count(self, word, category) :
+        if word in self.word_dict[category] :
+            return self.word_dict[category][word]
+        else :
+            return 0
+    
+    # 카테고리 계산
+    def category_prob(self, category) :
+        sum_categories = sum(self.category_dict.values())
+        category_v = self.category_dict[category]
+        return category_v / sum_categories
+
+    # 카테고리 내부의 단어 출현 비율 계산
+    def word_prob(self, word, category) :
+        n = self.get_word_count(word, category) + 1 
+        d = sum(self.word_dict[category].values()) + len(self.words)
+        return n / d
+        
     
 
 
