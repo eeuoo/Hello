6c91d8f5a31c5dc3c876ba20edc4e4d466d9e5d9
eeuoo <lee512joo@gmail.com>
2019-06-10 23:55:19 +0900

    2019-06-10

--- deep_learning/caltech101_keras2.py
+++ deep_learning/caltech101_keras2.py
@@ -23,4 +23,15 @@ for i, v in enumerate(pre) :
     img = Image.fromarray( np.unit8(dat) )
     img.save(fname)
 
+hdf5_file = './data/5obj_model.hdf5'
+
+if os.path.existx(hdf5_file) :
+    # 기존에 학습된 모델 읽어 들이기
+    model.load_weights(hdf5_file)
+
+else :
+    # 학습한 모델을 파일로 저장하기
+    model.fit(x_train, y_train, batch_size = 32, nb_epoch = 50)
+    model.save_weights(hdf5_file)
+
 
