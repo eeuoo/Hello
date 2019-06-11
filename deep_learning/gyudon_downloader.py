ec6101c9267fa82a0fc08c04e50046e662023de5
eeuoo <lee512joo@gmail.com>
2019-06-11 23:31:43 +0900

    2019-06-11

--- deep_learning/gyudon_downloader.py
+++ deep_learning/gyudon_downloader.py
@@ -29,4 +29,64 @@ def search_photo(keyword, offset = 0, limit = 100) :
     time.sleep(1)     # 1초 쉬기
 
     return json.load( open(cache, 'r', encoding='utf-8') )
-    
+
+# 이미지 다운로드
+def download_thumb(info, save_dir) :
+    if not os.path.exists(save_dir) :
+        os.makedirs(save_dir)
+
+    if info is None :
+        return
+
+    if not 'photo' in info['info'] :
+        print('[ERROR] broken info' )
+        return
+
+    photolist = info['info']['photo']
+
+    for photo in photolist :
+        title = photo['photo_title']
+        photo_id = photo['photo_id']
+        url = photo['thumbnail_image_url']
+        path = save_dir + '/' + str(photo_id) + '_thumb.jpg'
+
+        if os.path.exists(path) :
+            continue
+        try :
+            print('[download]', title, photo_id)
+            req.urlretrieve(url, path)
+            time.sleep(1)     # 1초 쉬기
+        except Exception as e :
+            print('[ERROR] failed to download url = ', url)
+
+# 모두 검색하고 다운로드하기
+def download_all(keyword, save_dir, maxphoto = 1000) :
+    offset = 0
+    limit = 100
+
+    while True :
+        # API 호출
+        info = search_photo(keyword, offset = offset, limit = limit)
+        if info is None :
+            print('[ERROR] no result')
+            return
+        if (not 'info' in info) or (not 'photo_num' in info['info']) :
+            print('[ERROR] broken data')
+            return
+
+        photo_num = info['info']['photo_num']
+
+        if photo_num == 0 :
+            print('photo_num = 0, offset = ', offset)
+            return
+        
+        # 사진 정보가 포함돼 있으면 다운받기
+        print('*** download offset = ', offset)
+        download_thumb(info, save_dir)
+        offset += limit
+        if offset >= maxphoto : 
+            break
+
+if __name__ == '__main__' :
+    # 모듈로 사용할 수 있게 설정
+    download_all(' ', './data/gyudon')
