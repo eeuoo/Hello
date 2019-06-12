d34e4cf6df88681b473b643729e81fc4c5854b56
eeuoo <lee512joo@gmail.com>
2019-06-12 23:48:49 +0900

    2019-06-12

--- deep_learning/gyudon_makedata.py
+++ deep_learning/gyudon_makedata.py
@@ -13,3 +13,25 @@ image_size = 50
 # 폴더마다의 이미지 데이터 읽어 들이기
 X = []    # 이미지 데이터
 Y = []    # 레이블 데이터
+
+for idx, cat in enumerate(categories) :
+    image_dir = root_dir + '/' + cat
+    files = glob.glob(image_dir + '/*.jpg')
+    print('-----', cat, '처리 중')
+
+    for i, f in enumerate(files) :
+        img = Image.open(f)
+        img = img.convert('RGB')   # 색상 모드 변경
+        img = img.resize( (image_size, image_size))   # 이미지 크기 변경
+        data = np.asarray(img)
+        X.append(data)
+        Y.append(idx)
+
+X = np.array(X)
+Y = np.array(Y)
+
+# 학습 전용 데이터와 텍스트 전용 데이터 분류하기
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
+xy = (X_train, X_test, y_train, y_test)
+np.save('./data/gyudon.npy', xy)
+print('ok', len(Y))
