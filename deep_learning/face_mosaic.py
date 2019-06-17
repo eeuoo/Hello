208d7fcb6429024ce0101801cab1d322e8b20070
eeuoo <lee512joo@gmail.com>
2019-06-17 22:45:15 +0900

    2019-06-17

--- deep_learning/face_mosaic.py
+++ deep_learning/face_mosaic.py
@@ -18,3 +18,32 @@ cascade_file = "haarcascade_frontalface_alt.xml"
 image = cv2.imread(image_file)
 image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # 그레이스케일 변환
 
+# 얼굴 인식 실행하기
+cascade = cv2.CascadeClassifier(cascade_file)
+face_list = cascade.detectMultiScale(image_gs,
+    scaleFactor = 1.1,
+    minNeighbors = 1,
+    minSize = (100, 100) )
+
+if len(face_list) == 0 :
+    print("no face")
+    quit()
+
+# 확인한 부분에 모자이크 걸기
+print(face_list)
+for (x, y, w, h) in face_list :
+    # 얼굴 부분 자르기
+    face_img = image[y : y+h, x : x+w]
+
+    # 자른 이미지를 지정한 배율로 확대/축소
+    face_img = cv2.resize(face_img, (w/mosaic_rate, h/mosaic_rate))
+
+    # 확대/축소한 그림을 원래 크기로 돌리기
+    face_img = cv2.resize(face_img, (w, h),
+        interpolation = cv2.INTER_AREA)
+
+    # 원래 이미지에 붙이기
+    image[y : y+h, x : x+w] = face_img
+
+# 렌더링 결과를 파일에 출력
+cv2.imwrite(output_file, image)
