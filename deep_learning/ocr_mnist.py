c501643e750bbbd7d789fe3dd3b6db0f5494da31
eeuoo <lee512joo@gmail.com>
2019-06-19 23:25:18 +0900

    2019-06-19

--- deep_learning/ocr_mnist.py
+++ deep_learning/ocr_mnist.py
@@ -37,4 +37,26 @@ def main() :
     score = model.evaluate(X_test, y_test, verbose = 0)
     print('socre = ', score)
 
+def build_model() :
+    # MLP 모델 구축
+    model = Sequential()
+    model = Sequential()
+    model.add( Dense(512, input_shape = (784,)) )
+    model.add( Activation('relu') )
+    model.add( Dropout(0.2) )
+    model.add( Dense(512) )
+    model.add( Activation('relu') )
+    model.add( Dropout(0.2) )
+    model.add( Dense(10) )
+    model.add( Activation('softmax') )
+
+    model.compile( loss = 'categorical_crossentropy',
+        optimizer = RMSprop(),
+        metrics = ['accuracy'])
+
+    return model
+
+if __name__ == '__main__' :
+    main()
+
 
